d1fc15ec843ef199cae6085569e640548b1e168d
patiphanz <patiphan.sr@ku.th>
2017-03-04 00:14:39 +0700

    edited exercise 2

---
+++ src/codeguide/Department.java
@@ -0,0 +1,5 @@
+package codeguide;
+
+public class Department {
+
+}
---
+++ src/codeguide/EmployeeManager.java
@@ -0,0 +1,23 @@
+package codeguide;
+
+/**
+ * Class of Employee in one Company
+ * @author patiphansrisook
+ *
+ */
+class Employee{
+	private Department department;
+	private Address address;
+	private Education education;
+}
+
+public class EmployeeManager {
+	
+	/**Get prepreffered address from your employee*/
+	public Address getPrefferedAddress(Employee e){
+		//Get all addresses from database
+        //Apply logic to determine which address is preferred
+        //Return address
+	}
+	
+}
